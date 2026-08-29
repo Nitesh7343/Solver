873c4dab2bc0407e37f0eda328b747cc5d0590b7
Nitesh7343 <niteshsingh7343@gmail.com>
2026-08-29 23:00:47 +0530

    Moves to rotate cube

---
+++ src/main/java/com/solver/round_solve/cube/Move.java
@@ -0,0 +1,27 @@
+package com.solver.round_solve.cube;
+
+public enum Move {
+    UP,
+    UP_PRIME,
+    UP2,
+
+    RIGHT,
+    RIGHT_PRIME,
+    RIGHT2,
+
+    FRONT,
+    FRONT_PRIME,
+    FRONT2,
+
+    DOWN,
+    DOWN_PRIME,
+    DOWN2,
+
+    LEFT,
+    LEFT_PRIME,
+    LEFT2,
+
+    BACK,
+    BACK_PRIME,
+    BACK2
+}
